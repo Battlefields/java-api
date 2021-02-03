ebc2dd8d039cff41c29fa55ced4dafcd168fbce1
Ocelot <FinntheRaider@gmail.com>
2021-02-02 18:11:26 -0800

    :sparkles: Updated Server Hostname

--- src/main/java/io/github/tastac/bfj/BFJ.java
+++ src/main/java/io/github/tastac/bfj/BFJ.java
@@ -8,7 +8,7 @@ package io.github.tastac.bfj;
 public class BFJ
 {
     public static final String BF_ICONS_URL = "https://raw.githubusercontent.com/Tastac/resources/master/battlefields";
-    public static final String BF_SERVER_HOSTNAME = "us-east.battlefieldsmc.net";
+    public static final String BF_SERVER_HOSTNAME = "us-west.battlefieldsmc.net";
     public static final String BF_API_URL = "https://api.battlefieldsmc.net/api/";
     public static final String BF_SERVER_STATUS_URL = BF_API_URL + "status/";
     public static final String BF_SERVER_INFO_URL = "https://api.mcsrvstat.us/2/" + BF_SERVER_HOSTNAME;
